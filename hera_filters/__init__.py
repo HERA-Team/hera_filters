12a3081bc846f99cc45e367e4c47a245d21d6ee1
Steven Murray <steven.g.murray@asu.edu>
2023-01-26 09:06:26 -0700

    maint: use setuptools_scm like the rest of the hera stack

--- hera_filters/__init__.py
+++ hera_filters/__init__.py
@@ -1,4 +1,15 @@
 from . import dspec
-from . import version
 
-__version__ = version.version
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version
+
+try:
+    from ._version import version as __version__
+except ModuleNotFoundError:  # pragma: no cover
+    try:
+        __version__ = version("hera_filters")
+    except PackageNotFoundError:
+        # package is not installed
+        __version__ = "unknown"
